f8c0445c894641181db3d99a4260c27a484e9fc4
Manikandan-P-M <p.m.manikndn@gmail.com>
2023-01-16 14:26:16 +0000

    music-playlist

---
+++ src/components/PlayList/index.js
@@ -0,0 +1,36 @@
+import {AiOutlineDelete} from 'react-icons/ai'
+import './index.css'
+
+const PlayList = props => {
+  const {data, onDelete} = props
+  const {id, imageUrl, name, genre, duration} = data
+
+  const oClickDelete = () => {
+    onDelete(id)
+  }
+
+  return (
+    <li className="list">
+      <div className="img-container">
+        <img src={imageUrl} alt="track" className="image" />
+        <div className="name-container">
+          <p className="name">{name}</p>
+          <p className="genre">{genre}</p>
+        </div>
+      </div>
+      <div className="time-container">
+        <p className="duration">{duration}</p>
+        <button
+          className="delete-btn"
+          type="button"
+          onClick={oClickDelete}
+          data-testid="delete"
+        >
+          <AiOutlineDelete color="white" size={16} />
+        </button>
+      </div>
+    </li>
+  )
+}
+
+export default PlayList
